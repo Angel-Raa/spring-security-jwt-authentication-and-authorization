a0f6dc8e02687eafeb48c9f9fae1d86bde0fbdcf
Angel Aguero <angelagueror23@gmail.com>
2023-10-03 20:42:48 -0400

    Dockerización de database y api
    * Se agregó un nuevo Dockerfile para la base de datos.
    * Se agregó un nuevo Dockerfile para la API.
    * Se agregó un nuevo archivo docker-compose.yml para orquestar los contenedores.

--- src/main/java/com/github/angel/raa/modules/configuration/security/HttpSecurityConfiguration.java
+++ src/main/java/com/github/angel/raa/modules/configuration/security/HttpSecurityConfiguration.java
@@ -7,6 +7,7 @@ import com.github.angel.raa.modules.utils.enums.PermissionEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
